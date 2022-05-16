31998ef344bce63e167882c15fe6cac519c55574
Agrippino <agrippinorussiello@outlook.it>
2022-05-16 15:23:33 +0200

    Creato DB

---
+++ PizzeriaAgrippino/Data/PizzaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PizzeriaAgrippino.Data
+{
+    public class PizzaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=AgrippinosPizzaDB;Integrated Security=True");
+        }
+    }
+}
+
--- PizzeriaAgrippino/Models/Pizze.cs
+++ PizzeriaAgrippino/Models/Pizze.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PizzeriaAgrippino.Models
 
@@ -6,6 +7,7 @@ namespace PizzeriaAgrippino.Models
     public class Pizze
     {
         //inseriamo required che obbliga all'utente di inserire quel campo o una determinata informazione
+        [Key]
         public int Id { get; set; }
         [Required(ErrorMessage="Il campo immagine è obbligatorio")]
         [Url(ErrorMessage = "Non hai inserito un Url valido")]
@@ -15,6 +17,7 @@ namespace PizzeriaAgrippino.Models
         public string NamePizza { get; set; }
         [Required(ErrorMessage = "Il campo Descrizione è obbligatorio")]
         [StringLength(250, ErrorMessage = "Hai superato il limire di caratteri massimo di 250")]
+        [Column(TypeName = "Text")]
         public string DescriptionPizza { get; set; }
         [Required(ErrorMessage = "Il campo Prezzo è obbligatorio")]
         [Range(0.01,30, ErrorMessage = "Mi dispaice hai inserito un prezzo non valido, i minimo è 0.01€ mentre il massimo è 30€ ")]
@@ -25,9 +28,8 @@ namespace PizzeriaAgrippino.Models
 
         }
 
-        public Pizze(int id, string imagePizza, string namePizza, string descriptionPizza, double pricePizza)
+        public Pizze(string imagePizza, string namePizza, string descriptionPizza, double pricePizza)
         {
-            this.Id = id;
             this.ImagePizza = imagePizza;
             this.NamePizza = namePizza;
             this.DescriptionPizza = descriptionPizza;
