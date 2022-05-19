eae2184b6d4cf988f9c0bba4e1f35e50bafd35cc
Agrippino <agrippinorussiello@outlook.it>
2022-05-19 12:17:02 +0200

    Inizio cambiamento con implementazione API cancellata pivacy e inizio modifica su index

---
+++ PizzeriaAgrippino/Controllers/Api/Pizzerias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzeriaAgrippino.Data;
+using PizzeriaAgrippino.Models;
+
+namespace PizzeriaAgrippino.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Pizzerias : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+           List<Pizze> pizzes = new List<Pizze>();
+
+            using (PizzaContext DatabasePizza = new PizzaContext())
+            {
+                pizzes = DatabasePizza.Pizzas.ToList<Pizze>();
+            }
+            
+            return Ok(pizzes);
+        }
+    }
+}
