abfae151277cb000401c39f36acf132cede1e870
Agrippino <agrippinorussiello@outlook.it>
2022-05-19 21:03:10 +0200

    Sto cercando di fare il bonus creando il metodo per aggiungere le pizze ai preferiti

---
+++ PizzeriaAgrippino/Controllers/Api/FavouritesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzeriaAgrippino.Data;
+using PizzeriaAgrippino.Models;
+
+namespace PizzeriaAgrippino.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FavouritesController : ControllerBase
+    {
+        [HttpPost]
+
+        public IActionResult AddToFavourite(int id)
+        {  
+            List<Pizze> MyFavouritePizza = new List<Pizze>();
+
+            using (PizzaContext DatabasePizza = new PizzaContext())
+                {
+                    Pizze AddFavourtie = DatabasePizza.Pizzas
+                       .Where(Pizze => Pizze.Id == id)
+                       .First();
+
+                    if (AddFavourtie != null)
+                    {
+                        MyFavouritePizza.Add(AddFavourtie);
+                        
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            
+
+        }
+    }
+}
